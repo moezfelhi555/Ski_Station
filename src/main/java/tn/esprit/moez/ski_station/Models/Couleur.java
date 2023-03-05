14305f2cb6be5e09b4331b5b662c51255e7f0939
moez <moez.felhi@esprit.tn>
2023-03-05 23:39:01 +0100

    crud

--- src/main/java/tn/esprit/moez/ski_station/Models/Couleur.java
+++
@@ -1,5 +0,0 @@
-package tn.esprit.moez.ski_station.Models;
-
-public enum Couleur {
-    VERT,BLEU,ROUGE,NOIR
-}
--- src/main/java/tn/esprit/moez/ski_station/Models/TypeCours.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.moez.ski_station.Models;
-
-public enum TypeCours {
-    COLLECTIF_ENFANT,
-    COLLECTIF_ADULTE,
-    PARTICULIER
-}
